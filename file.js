1aa85b4905f5fc1f945521fa0a91d51e5f20d9a8
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-08-05 14:04:16 +0000

    create multiple users

--- file.js
+++ file.js
@@ -1072,3 +1072,76 @@
 // const func= person.about.bind(person,"paino","arijit");
 
 // func()
+
+/**********************create multiple users********************** */
+
+// function createUser(firstName,lastName,age,email,address){
+//     const user={};
+//     user.firstName=firstName;
+//     user.lastName=lastName;
+//     user.age=age;
+//     user.email=email;
+//     user.address=address;
+
+//     user.about=function(){
+//         return `${this.firstName} is ${this.age} year old.`;
+//     }
+//     user.is18=function(){
+//         return this.age>=18;
+//     }
+//     return user;
+// }
+
+// const user1=createUser("hanzala","shaik",18,"shaikhhanzala@gmail.com","myaddress");
+// console.log(user1);
+
+// console.log(user1.about());
+
+// console.log(user1.is18());
+
+/******Efficient****** */
+
+// const userMethods={
+//    about:function(){
+//         return `${this.firstName} is ${this.age} year old.`;
+//     },
+//    is18:function(){
+//         return this.age>=18;
+//     }
+    
+// }
+
+// function createUser(firstName,lastName,age,email,address){
+//     const user={};
+//     user.firstName=firstName;
+//     user.lastName=lastName;
+//     user.age=age;
+//     user.email=email;
+//     user.address=address;
+//     user.about=userMethods.about;
+//     user.is18=userMethods.is18;
+
+//     return user;
+// }
+
+// const user1=createUser("hanzala","shaik",18,"shaikhhanzala@gmail.com","myaddress");
+// console.log(user1);
+
+// console.log(user1.about());
+
+// console.log(user1.is18());
+
+/******************proto/prototype*******************/
+
+const obj1={
+    key1:"value1",
+    key2:"value2"
+}
+
+const obj2=Object.create(obj1);
+
+obj2.key3="value3";
+
+console.log(obj2.key1);
+console.log(obj2);
+
