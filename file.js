5601718b3d6dbfc418e8893c0c10ff070bc56512
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-08-08 08:42:52 +0000

    getter and setter

--- file.js
+++ file.js
@@ -1328,4 +1328,30 @@
 // const info=Person.classInfo();
 // console.log(info);
 
+/*******************Getter and Setter********************/
+
+class Person{
+    constructor(firstName,lastName,age){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+    set fullName(fullName){
+        const [firstName ,lastName]=fullName.split(" ");
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+}
+
+const person1=new Person("hanzala","shaik",18);
+console.log(person1);
+console.log(person1.fullName);
+person1.fullName="shaik hanzala"
+console.log(person1.fullName);
+
+
 
